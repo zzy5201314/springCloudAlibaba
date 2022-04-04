492cf4521bfc5262b707b1b65180558427d72319
zzy5201314 <1971166454@qq.com>
2022-04-04 21:48:23 +0800

    1.加入 springBoot Admin监控，使用元数据管理接入，并需要暴露端点才能使用 2.将 Nacos 接入 Admin监控 metadata:           management:             context-path: ${server.servlet.context-path}/actuator
     # 暴露端点( actuator 监控使用)
     management:
       endpoints:
         web:
           exposure:
             include: '*'
       endpoint:
         health:
           show-details: always

--- e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/NacosClientApplication.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/NacosClientApplication.java
@@ -3,6 +3,7 @@ package com.imooc.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * <h1>Nacos Client 工程启动入口</h1>
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author zzy
  * @date 2022/4/4
  */
+@RefreshScope   //  刷新配置
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosClientApplication {

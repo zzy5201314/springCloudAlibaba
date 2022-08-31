92b52ae7281f3e8ec6b42935fb53c501424425b5
zzy5201314 <1971166454@qq.com>
2022-08-31 09:23:47 +0800

    修改注释

--- e-commerce-service/e-commerce-goods-service/src/main/java/com/imooc/ecommerce/constant/GoodsConstant.java
+++ e-commerce-service/e-commerce-goods-service/src/main/java/com/imooc/ecommerce/constant/GoodsConstant.java
@@ -2,12 +2,13 @@ package com.imooc.ecommerce.constant;
 
 /**
  * TODO: 商品常量信息
+ * TODO: Redis key
  *
  * @author zzy
  * @date 2022/8/25
  */
 public class GoodsConstant {
 
-    /** TODO: Redis key */
-    public static final String ECOMMERCE_GOODS_DICT_KEY = "ecommerce:goods:dict:20220825";
+    /** TODO: 简单商品信息 */
+    public static final String ECOMMERCE_GOODS_DICT_KEY = "ecommerce:simpleGoodsInfo:dict:20220825";
 }
